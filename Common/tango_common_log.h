56123a89edb963d0f431ffe2920ea0294a66a191
Michele Devetta <michele.devetta@mail.polimi.it>
2018-04-17 14:32:44 +0200

    Added possibility to log DevErrorList objects (useful in event handlers)

--- Common/tango_common_log.h
+++ Common/tango_common_log.h
@@ -10,43 +10,59 @@
 #include <sstream>
 
 
-inline void error_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+inline void error_log_errorlist(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevErrorList &errors) {
   std::stringstream msg;
   msg << std_msg << endl;
-  for(size_t i = 0; i < e.errors.length(); i++) {
-    msg << "[" << (i+1) << "] " << e.errors[i].origin << ": " << e.errors[i].desc << endl;
+  for(size_t i = 0; i < errors.length(); i++) {
+    msg << "[" << (i+1) << "] " << errors[i].origin << ": " << errors[i].desc << endl;
   }
   dev->get_logger()->error_stream() << log4tango::LogInitiator::_begin_log << msg.str();
 }
 
+inline void error_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+  error_log_errorlist(dev, std_msg, e.errors);
+}
 
-inline void warn_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+
+inline void warn_log_errorlist(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevErrorList &errors) {
   std::stringstream msg;
   msg << std_msg << endl;
-  for(size_t i = 0; i < e.errors.length(); i++) {
-    msg << "[" << (i+1) << "] " << e.errors[i].origin << ": " << e.errors[i].desc << endl;
+  for(size_t i = 0; i < errors.length(); i++) {
+    msg << "[" << (i+1) << "] " << errors[i].origin << ": " << errors[i].desc << endl;
   }
   dev->get_logger()->warn_stream() << log4tango::LogInitiator::_begin_log << msg.str();
 }
 
+inline void warn_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+  warn_log_errorlist(dev, std_msg, e.errors);
+}
 
-inline void info_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+
+inline void info_log_errorlist(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevErrorList &errors) {
   std::stringstream msg;
   msg << std_msg << endl;
-  for(size_t i = 0; i < e.errors.length(); i++) {
-    msg << "[" << (i+1) << "] " << e.errors[i].origin << ": " << e.errors[i].desc << endl;
+  for(size_t i = 0; i < errors.length(); i++) {
+    msg << "[" << (i+1) << "] " << errors[i].origin << ": " << errors[i].desc << endl;
   }
   dev->get_logger()->info_stream() << log4tango::LogInitiator::_begin_log << msg.str();
 }
 
+inline void info_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+  info_log_errorlist(dev, std_msg, e.errors);
+}
 
-inline void debug_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+
+inline void debug_log_errorlist(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevErrorList &errors) {
   std::stringstream msg;
   msg << std_msg << endl;
-  for(size_t i = 0; i < e.errors.length(); i++) {
-    msg << "[" << (i+1) << "] " << e.errors[i].origin << ": " << e.errors[i].desc << endl;
+  for(size_t i = 0; i < errors.length(); i++) {
+    msg << "[" << (i+1) << "] " << errors[i].origin << ": " << errors[i].desc << endl;
   }
   dev->get_logger()->debug_stream() << log4tango::LogInitiator::_begin_log << msg.str();
+}
+
+inline void debug_log_exception(Tango::DeviceImpl *dev, const char* std_msg, Tango::DevFailed &e) {
+  debug_log_errorlist(dev, std_msg, e.errors);
 }
 
 
